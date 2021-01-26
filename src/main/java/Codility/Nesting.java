f5cb1bcf09fcce1e5afe69f596d91b9619da205a
Agata <pietrucha.piotrek@gmail.com>
2021-01-26 22:26:13 +0100

    first commit

---
+++ src/main/java/Codility/Nesting.java
@@ -0,0 +1,32 @@
+package Codility;
+
+public class Nesting {
+    public int solution(String S) {
+        if (S == null || S.equals("")) {
+            return 1;
+        }
+        char[] tab = S.toCharArray();
+        if (tab[0] == '(') {
+
+            int sumOfRightNesting = 0;
+            int sumOfLeftNesting = 0;
+            for (int i = 0; i < tab.length; i++) {
+                if((sumOfRightNesting - sumOfLeftNesting) < 0) return 0;
+
+                if (tab[i] == '(') {
+                    sumOfRightNesting += 1;
+                }
+                if (tab[i] == ')') {
+                    sumOfLeftNesting += 1;
+                }
+            }
+            if (sumOfRightNesting == sumOfLeftNesting) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
+}

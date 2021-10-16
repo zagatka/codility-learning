c10cd722d135bb16a6916ab6a4a4579f9b9fdf6f
Agata <stanek.agatka@gmail.com>
2021-10-16 19:20:40 +0200

    ParkingBill (recursion)

--- src/main/java/Codility/ParkingBill.java
+++ src/main/java/Codility/ParkingBill.java
@@ -1,6 +1,14 @@
 package Codility;
 
 public class ParkingBill {
+
+    //You parked your car in a parking lot and want to compute the total cost of the ticket.
+    // The billing rules are as follows:
+    //
+    //        The entrance fee of the car parking lot is 2;
+    //        The first full or partial hour costs 3;
+    //        Each successive full or partial hour (after the first) costs 4.
+
     public int solution(String E, String L) {
         int minParking = calculateMinutes(L) - calculateMinutes(E);
         return pay(minParking);
